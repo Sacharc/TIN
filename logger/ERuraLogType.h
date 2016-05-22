6e5b4eb9448da564b7856d5b47aa48de99f15390
jdudziak94 <jdudziak@mion.elka.pw.edu.pl>
2016-05-22 10:40:03 -0700

    Enum LogType

---
+++ logger/ERuraLogType.h
@@ -0,0 +1,14 @@
+//
+// Created by jdudziak on 5/22/16.
+//
+
+#ifndef TIN_ERURALOGTYPE_H
+#define TIN_ERURALOGTYPE_H
+
+enum class ERuraLogType{
+    LOGGER_ERROR,
+    LOGGER_WARNING,
+    LOGGER_INFO,
+    LOGGER_DEBUG
+};
+
