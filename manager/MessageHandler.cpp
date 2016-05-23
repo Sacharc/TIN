4e879c8eec857903fc8e0d3fbee1a5687fde6216
Sacharc <mi3uss@gmail.com>
2016-05-23 19:22:19 +0200

    Message handler

---
+++ manager/MessageHandler.cpp
@@ -0,0 +1,26 @@
+//
+// Created by msacharc on 23.05.16.
+//
+#include "MessageHandler.h"
+
+void MessageHandler::handle(struct message *msg) {
+
+    switch (msg->msg_type) {
+        case REPORT:
+            printf("Detector %ld reported: waterlevel: %d/%d\n", msg->id, msg->currentResistance,
+                   msg->typicalResistance);
+            break;
+        case ALARM:
+            printf("Detector %ld ALARMED: waterlevel: %d/%d\n", msg->id, msg->currentResistance,
+                   msg->typicalResistance);
+            break;
+        deafult:
+            printf("Unknown message from detector %ld\n", msg->id);
+            break;
+    }
+
+    time_t  timev;
+    time(&timev);
+    history.add(msg->id, new HistoryRecord(*msg, timev));
+
+}
