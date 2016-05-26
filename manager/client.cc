9705b009131c79c469514c533c7588e24c075d42
Paweł <pawel.jonski9@gmail.com>
2016-05-26 16:09:43 +0200

    DETECTOR szuka po wszystkich 192.168.0.X, ale zajmuje mu duzo czasu sprawdzenie polaczenia. TODO: Dodac timeout oraz przetestowac poprawnosc z maszynami wirtualnymi

--- manager/client.cc
+++ manager/client.cc
@@ -7,6 +7,8 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "../common/messageType.h"
 #include "../common/message.h"
 #include "MessageHandler.h"
@@ -14,23 +16,28 @@
 
 
 #define DET_NMB 3
-
+#define MAX_SOCK 20
 int main(int argc, char *argv[])
 {
-	if(argc<3)
+	if(argc<1)
 	{
-		printf("Usage:\n ./MANAGER <DETECTOR_IP> <DETECTOR_PORT>\n");
+		printf("Usage:\n ./MANAGER <DETECTOR_IP>\n");
 		return 0;
 	}
 /* Variables */
 	fd_set readfds;
 	FD_ZERO (&readfds);
-	int sock[2];
+	int sock[20];
 	struct hostent *hp;
 	struct message *msg;
 	msg = (struct message*)malloc(sizeof(struct message));
 
     MessageHandler handler;
+<<<<<<< HEAD
+	struct sockaddr_in servers [20];
+	int ip[] = {1,0,168,192};
+	char buf[16];
+=======
 	struct sockaddr_in servers [2];
 
 /* Create http handler thread */
@@ -42,53 +49,57 @@ int main(int argc, char *argv[])
 	else
 		printf("Http thread created successfully\n");
 
+>>>>>>> afc8401e2bd3062159f37136462fe6286c9987f5
 /* Create socket*/
+int servcount = 0;
 	int i=0;
-	for(i=0; i<DET_NMB; i++)
+	for(i=1; i<255; i++,ip[0]++)
 	{
 		printf("Client started\n");
-		sock[i]=socket(AF_INET, SOCK_STREAM, 0);
-		if(sock[i]<0)
+		sock[servcount]=socket(AF_INET, SOCK_STREAM, 0);
+		if(sock[servcount]<0)
 		{
-			perror("Failed to create socket");
-			close(sock[i]);
-			exit(1);
+			perror("Failed to create socket.\n");
+			close(sock[servcount]);
+			continue;
 		}
 		printf("Socket created.\n");
-		servers[i].sin_family = AF_INET;
-		hp= gethostbyname (argv[1+i*2]);
+		servers[servcount].sin_family = AF_INET;
+		snprintf(buf, 16, "%d.%d.%d.%d", ip[3],ip[2],ip[1],ip[0]);
+		hp= gethostbyname (buf);
 		if(hp == 0)
 		{
-			perror("GetHostName failed");
-			close(sock[i]);
-			exit(1);
+			perror("GetHostName failed. \n");
+			close(sock[servcount]);
+			continue;
 		}
 		printf("Hostname received.\n");
 
-		memcpy(&servers[i].sin_addr, hp->h_addr, hp->h_length);
-		servers[i].sin_port = atoi(argv[2+i*2]);
+		memcpy(&servers[servcount].sin_addr, hp->h_addr, hp->h_length);
+		servers[servcount].sin_port = 4000;
+		printf("Address: %s\n",inet_ntoa(servers[servcount].sin_addr));
 
-
-		if(connect(sock[i], (struct sockaddr*) &servers[i], sizeof (servers[i])) < 0)
+		if(connect(sock[servcount], (struct sockaddr*) &servers[servcount], sizeof (servers[servcount])) < 0)
 		{
 			perror("Connect failed");
-			close(sock[i]);
-			exit(1);
+			close(sock[servcount]);
+			continue;
 		}
 		printf("Successfully connected to: %s\n",hp->h_name);
+		servcount++;
 	}
 	int end = 0;
 	int rval;
 	while(end != 1)
 	{
 		int j=0;
-		for(j=0; j< DET_NMB; j++)
+		for(j=0; j< servcount; j++)
 		{
 			FD_SET(sock[j],&readfds);
 		}
 		i = 0;
 		select(FD_SETSIZE, &readfds,NULL,NULL,NULL);
-		for (i=0; i<DET_NMB; i++)
+		for (i=0; i<servcount; i++)
 		{
 			if(FD_ISSET(sock[i],&readfds))
 			{
@@ -105,8 +116,10 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-	close(sock[0]);
-	close(sock[1]);
+	for (i=0;i<servcount;i++)
+	{
+		close(sock[i]);
+	}
 	free(msg);
 	return 0;
 }
