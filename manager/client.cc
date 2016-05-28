47da2f5257b6137ba5062ee2a9cc9d0851f6b3ec
sacharc <mi3uss@gmail.com>
2016-05-28 12:24:47 +0200

    Styl

--- manager/client.cc
+++ manager/client.cc
@@ -17,9 +17,9 @@
 #include "MessageHandler.h"
 #include "../http_module/HttpHandler.h"
 
+const char* eRuraSubnet = "192.168.1";
+const int eRuraPortNumber = 2137;
 
-#define DET_NMB 3
-#define MAX_SOCK 20
 int main(int argc, char *argv[])
 {
     if(argc<1)
@@ -30,7 +30,6 @@ int main(int argc, char *argv[])
 /* Variables */
     fd_set readfds;
     FD_ZERO (&readfds);
-    int sock[MAX_SOCK];
     struct hostent *hp;
     struct message *msg;
     struct timeval times;
@@ -40,97 +39,82 @@ int main(int argc, char *argv[])
 
     MessageHandler handler;
 
-    struct sockaddr_in servers [20];
-  	int ip[] = {1,0,168,192};
-  	char buf[16];
-
 /* Create http handler thread */
-    std::thread httpHandler(httpHandlerStart, &handler);
+//    std::thread httpHandler(httpHandlerStart, &handler);
 
-    /* Create socket*/
-    int servcount = 0;
-    	int i=0;
-    	for(i=1; i<255; i++)
-    	{
-    		fd_set fds_connect;
-    		FD_ZERO (&fds_connect);
-    		ip[0] = i;
-    		//printf("Client started\n");
-    		sock[servcount]=socket(AF_INET, SOCK_STREAM, 0);
-    		fcntl(sock[servcount], F_SETFL, O_NONBLOCK);
-    		if(sock[servcount]<0)
-    		{
-    			//perror("Failed to create socket.\n");
-    			close(sock[servcount]);
-    			continue;
-    		}
-    		//printf("Socket created.\n");
-    		servers[servcount].sin_family = AF_INET;
-    		snprintf(buf, 16, "%d.%d.%d.%d", ip[3],ip[2],ip[1],ip[0]);
-    		hp= gethostbyname (buf);
-    		if(hp == 0)
-    		{
-    			//perror("GetHostName failed. \n");
-    			close(sock[servcount]);
-    			continue;
-    		}
-    	//	printf("Hostname received.\n");
+    std::vector<int> sockets;
+    //  TODO wydaje mi sie ze nie potrzebne do przetestowania
+    std::vector<struct sockaddr_in> servs;
 
-    		memcpy(&servers[servcount].sin_addr, hp->h_addr, hp->h_length);
-    		servers[servcount].sin_port = 2137;
-    		printf("Address: %s\n",inet_ntoa(servers[servcount].sin_addr));
-    		int ret = connect(sock[servcount], (struct sockaddr*) &servers[servcount], sizeof (servers[servcount]));
-    		fd_set fd;
-    		FD_ZERO(&fd);
-    		FD_SET(sock[servcount],&fd);
-    		ret = select(FD_SETSIZE, NULL, &fd, NULL, &times);
-    		times.tv_usec=10000;
-    		if(ret <1)
-    		{
-    			printf("Connect failed!\n");
-    			close(sock[servcount]);
-    			continue;
-    		}
-    		printf("Successfully connected to: %s\n",hp->h_name);
-    		servcount++;
-    	}
-    	int end = 0;
-    	int rval;
-    	int j=0;
-    	for(j=0; j< servcount; j++)
-    	{
-    		FD_SET(sock[j],&readfds);
-    	}
-    	sleep(1);
-    	while(end != 1)
-    	{
-
-    		i = 0;
-    		select(FD_SETSIZE, &readfds,NULL,NULL,NULL);
-    		for (i=0; i<servcount; i++)
-    		{
-    			if(sock[i]!=-1 && FD_ISSET(sock[i],&readfds))
-    			{
-    				if((rval=recv(sock[i], msg, sizeof(struct message), 0)) <0) {
-    					perror("reading stream message error");
-    					FD_CLR(sock[i],&readfds);
-    					sock[i]=-1;
-    				}
-    				else if(rval==0)
-    				{
-    					printf("Ending connection\n");
-    					end=1;
-    				}
-    				else {
-                        handler.handle(msg);
-                    }
-    			}
-    		}
-    	}
-    	for (i=0;i<servcount;i++)
-    	{
-    		close(sock[i]);
-    	}
-    	free(msg);
-    	return 0;
+    for (int i = 1; i < 255; i++) {
+        char buf[16];
+        struct sockaddr_in serv;
+        fd_set fds_connect;
+        FD_ZERO (&fds_connect);
+        //printf("Client started\n");
+        int soc = socket(AF_INET, SOCK_STREAM, 0);
+        fcntl(soc, F_SETFL, O_NONBLOCK);
+        if(soc<0) {
+            close(soc);
+            continue;
+        }
+        serv.sin_family = AF_INET;
+        snprintf(buf, 16, "%s.%d", eRuraSubnet, i);
+        hp = gethostbyname (buf);
+        if (hp == 0) {
+            close(soc);
+            continue;
+        }
+        memcpy(&serv.sin_addr, hp->h_addr, hp->h_length);
+        serv.sin_port = eRuraPortNumber;
+        printf("Address: %s\n",inet_ntoa(serv.sin_addr));
+        connect(soc, (struct sockaddr*) &serv, sizeof (serv));
+        fd_set fd;
+        FD_ZERO(&fd);
+        FD_SET(soc,&fd);
+        times.tv_usec=10000;
+        if (select(FD_SETSIZE, NULL, &fd, NULL, &times) <1)
+        {
+            printf("Connect failed!\n");
+            close(soc);
+            continue;
+        }
+        printf("Successfully connected to: %s\n",hp->h_name);
+        sockets.push_back(soc);
+        servs.push_back(serv);
     }
+    int end = 0;
+    int rval;
+    for(unsigned i = 0; i < sockets.size(); i++) {
+        FD_SET(sockets[i],&readfds);
+    }
+    sleep(1);
+
+    while (end != 1) {
+        select(FD_SETSIZE, &readfds,NULL,NULL,NULL);
+        for (unsigned i = 0; i < sockets.size(); i++)
+        {
+            if(sockets[i]!=-1 && FD_ISSET(sockets[i], &readfds))
+            {
+                if((rval=recv(sockets[i], msg, sizeof(struct message), 0)) <0) {
+                    perror("reading stream message error");
+                    FD_CLR(sockets[i],&readfds);
+                    sockets[i]=-1;
+                }
+                else if(rval==0)
+                {
+                    printf("Ending connection\n");
+                    end=1;
+                }
+                else {
+                    handler.handle(msg);
+                }
+            }
+        }
+    }
+    for (unsigned i = 0; i < sockets.size(); i++) {
+        close(sockets[i]);
+    }
+    free(msg);
+    return 0;
+}
